1667903a5d7412ea3f75468a522d1006d3a789e9
yazandisi <yazandisi@gmail.com>
2022-05-19 19:35:00 -0500

    adding queries

--- queries.sql
+++ queries.sql
@@ -1,1 +1,26 @@
--- write your queries here
+-- JOINS Exercise
+-- Query 1
+SELECT * FROM owners o 
+  FULL OUTER JOIN vehicles v 
+    ON o.id=v.owner_id;
+
+-- Query 2
+SELECT first_name, last_name, 
+  COUNT(owner_id) FROM owners o 
+  JOIN vehicles v on o.id=v.owner_id 
+  GROUP BY (first_name, last_name) 
+ORDER BY first_name;
+
+
+-- Query 3
+SELECT 
+  first_name, last_name, 
+  ROUND(AVG(price)) as average_price, 
+  COUNT(owner_id) 
+FROM owners o 
+JOIN vehicles v on o.id=v.owner_id 
+GROUP BY 
+  (first_name, last_name) 
+HAVING 
+  COUNT(owner_id) > 1 AND ROUND(AVG(price)) > 10000 
+ORDER BY first_name DESC;
